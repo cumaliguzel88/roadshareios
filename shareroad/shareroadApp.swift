6179cd91a175a155b802b8fc0dfc9460724edbfe
CUMALİ GÜZEL <cumali.guzel@std.yeditepe.edu.tr>
2026-01-13 19:05:08 +0300

    Initial Commit

---
+++ shareroad/shareroadApp.swift
@@ -0,0 +1,17 @@
+//
+//  shareroadApp.swift
+//  shareroad
+//
+//  Created by Cumali Güzel on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct shareroadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
